1cb6f748421d21e8f96c222b2cd8928a51282d32
jessica <papylotomba@yahoo.fr>
2020-06-02 21:21:47 -0400

    getWorkoutsInRange() GET /api/workouts/range

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -59,3 +59,19 @@ router.post("/workouts", ({ body }, res) => {
 
     res.send('Got a POST request')
 });
+
+// getWorkoutsInRange() GET /api/workouts/range
+
+router.get("/workouts/range", (req, res) => {
+    db.Workout.find({}).limit(7)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+
+
+module.exports = router;
